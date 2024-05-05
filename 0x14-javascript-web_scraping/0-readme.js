e4c138cfdebb8027f171ab917087ba0f6d2573c5
DannieCodjoe <daniellacodjoe23@gmail.com>
2024-05-05 18:28:34 +0000

    task 0-readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,22 +1,16 @@
 #!/usr/bin/node
 // A script that reads and prints content of a file
 
-const fs = require('node:fs');
+#!/usr/bin/node
 
-const args = process.argv;
+const { argv } = require('node:process');
+const fs = require('fs');
 
-const filePath = args[2];
+const file = argv[2];
 
-if (!filePath) {
-  console.log('Usage: node 0-readme.js <file_path>');
-  process.exit(1);
-}
-
-fs.readfile(filePath, 'utf-8', (err, data) => {
+fs.readFile(file, 'utf8', (err, text) => {
   if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
+    console.log(err);
   }
+  console.log(text);
 });
-
